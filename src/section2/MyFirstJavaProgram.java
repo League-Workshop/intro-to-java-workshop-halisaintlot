bba3eff392530f65e5fae8078544065db05f4625
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-14 17:52:14 -0700

    Day 1

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,22 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot jim= new Robot ();
+	jim.setSpeed(100);
+	jim.penDown();
+	jim.setPenColor(Color.BLUE);
+	for (int i = 0; i < 4; i++) {
+		
 	
-		
-		
+	
+		jim.move(100);
+		jim.turn(90);
+	}
+		jim.sparkle();
+	
+	
+	
+	
+	
 	}
 }
