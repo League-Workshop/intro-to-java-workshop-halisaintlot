5365f840ae81412159e47aae62796a7346817670
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-28 19:04:22 -0700

    Day 3

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,48 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int randNum = new Random ().nextInt(4);
 	// 3. Print out this variable
 
 	// 4. Get the user to enter something that they think is awesome
-
+String awesome=JOptionPane.showInputDialog("Enter something you think is awesome.");
 	// 5. If the random number is 0
+if (randNum==0
+		) {
+	JOptionPane.showMessageDialog(null, awesome+" is awesome");
 
 	// -- tell the user whatever they entered is awesome!
-
+}else if (randNum==1)
 	// 6. If the random number is 1
-
+{
+	
+	JOptionPane.showMessageDialog(null, awesome+" is ok");
+}
 	// -- tell the user whatever they entered is ok.
 
 	// 7. If the random number is 2
-
+if (randNum==2)
+{
+	JOptionPane.showMessageDialog(null, awesome+" is boring");
+}
 	// -- tell the user whatever they entered is boring.
 
 	// 8. If the random number is 3
-
+if (randNum==3)
+{
+	JOptionPane.showMessageDialog (null, awesome+" is boring");
+}
 	// -- write your own answer
-
+}
 }
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,31 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
-	
+
 	public static void main(String[] args) {
-		
-		// 1.  Create a variable to hold the user's score 
-		
-		// 2.  Ask the user a question 
-		
-		// 3.  Use an if statement to check if their answer is correct
-		
-		// 4.  if the user's answer is correct
-		
-		// -- add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
+
+		// 1. Create a variable to hold the user's score
+		int score = 0;
+		// 2. Ask the user a question
+		String answer = JOptionPane.showInputDialog(null, "What is 100+100?");
+		// 3. Use an if statement to check if their answer is correct
+		if (answer.equals("200")) {
+			// 4. if the user's answer is correct
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "Correct");
+		}
+		// -- add one to their score
+
+		// 5. Create more questions by repeating steps 1, 2, and 3 below.
+
+		String answer2 = JOptionPane.showInputDialog(null, "How many seconds are in a minute?");
+		if (answer2.equals("60")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "Correct");
+		}
+		// 6. After all the questions have been asked, print the user's score
+		JOptionPane.showMessageDialog(null, "Your score is "+score);
 	}
 }
